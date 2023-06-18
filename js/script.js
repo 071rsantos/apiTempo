c6bb3c19ac6ae4b317498920e81f4d3fc6e4d721
Rodrigo Santos <santos.rodrigo.c7@gmail.com>
2023-06-18 15:04:35 -0300

    apiTempo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const key = '93c47d3f7c2876386a374f6b27759a0b'
+const botao = document.querySelector('.botao-busca')
+const cidadetela = document.querySelector('.cidade')
+const tempo = document.querySelector('.temp')
+const imagem = document.querySelector('.img-previsao')
+const textprev = document.querySelector('.texto-previsao')
+const umidade = document.querySelector('.umidade')
+
+botao.addEventListener('click', clicknobotao)
+
+function clicknobotao(){
+    const cidade = document.querySelector('.input-cidade').value
+    console.log(cidade)
+    buscarCidade(cidade)
+}
+
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then(resposta=>resposta.json())
+
+    console.log(dados)
+
+    dadosnatela(dados)
+
+}
+
+function dadosnatela(dados){
+    cidadetela.innerHTML = dados.name
+    tempo.innerHTML = Math.ceil(dados.main.temp) + " °C"
+    imagem.src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    textprev.innerHTML = dados.weather[0].description
+    umidade.innerHTML = "Umidade: " + dados.main.humidity + "%"
+}
